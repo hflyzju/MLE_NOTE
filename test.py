5bf7d8127341d630cfa2596d6f5b22fa85a94869
feynmanhu <hxshineuestc@gmail.com>
2022-10-29 10:57:31 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import torch
+a = torch.tensor([2., 3.], requires_grad=True)
+b = torch.tensor([6., 4.], requires_grad=True)
+Q = 3*a**3 - b**2
+external_grad = torch.tensor([2., 1.])
+Q.backward(gradient=external_grad)
+# check if collected gradients are correct
+print(9*a**2 == a.grad)
+print(-2*b == b.grad)
+print(a.grad)
